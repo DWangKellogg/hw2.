eaadd6301ddf17f6475e3ff7e9ff6554c1ad023b
DWangKellogg <dande.wang@kellogg.northwestern.edu>
2022-02-09 02:12:33 +0000

    submitting assignment

--- kmdb.rb
+++ kmdb.rb
@@ -94,168 +94,171 @@ Role.destroy_all
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
+#nolans = Artist.where({ name: "Christopher Nolan"}).[0]
+nolan = Artist.new
+nolan.name = "Christopher Nolan"
+nolan.save
+
 bb = Movie.new
 bb.title = "Batman Begins"
 bb.year_released = 2005
 bb.rating = "PG-13"
-bb.artist_id = 1 #write code association to link (has many & belong)
+bb.artist_id = nolan.id #write code association to link (has many & belong)
 bb.save
 
 dk = Movie.new
 dk.title = "The Dark Knight"
 dk.year_released = 2008
 dk.rating = "PG-13" 
-dk.artist_id = 1 #nolan.id #write code association to link (has many & belong)
+dk.artist_id = nolan.id #nolan.id #write code association to link (has many & belong)
 dk.save
 
 dkr = Movie.new
 dkr.title = "The Dark Knight Rises"
 dkr.year_released = 2012
 dkr.rating = "PG-13" 
-dkr.artist_id = 1 #nolan.id #write code association to link (has many & belong)
+dkr.artist_id = nolan.id #nolan.id #write code association to link (has many & belong)
 dkr.save
 
-#nolans = Artist.where({ name: "Christopher Nolan"}).[0]
-nolan = Artist.new
-nolan.name = "Christopher Nolan"
-nolan.save
 
 #bb
 bale = Artist.new
 bale.name = "Christian Bale"
 bale.save
 
+waynebb = Role.new
+waynebb.movie_id = bb.id #movie_id
+waynebb.artist_id = bale.id #artist_id
+waynebb.character_name = "Bruce Wayne"
+waynebb.save
+
 caine = Artist.new
 caine.name = "Michael Caine"
 caine.save
+
+alfredbb = Role.new
+alfredbb.movie_id = bb.id #movie_id
+alfredbb.artist_id = caine.id #artist_id
+alfredbb.character_name = "Alfred"
+alfredbb.save
 
 neeson = Artist.new
 neeson.name = "Liam Neeson"
 neeson.save
 
+ghulbb = Role.new
+ghulbb.movie_id = bb.id #movie_id
+ghulbb.artist_id = neeson.id #artist_id
+ghulbb.character_name = "Ra's Al Ghul"
+ghulbb.save
+
 holmes = Artist.new
 holmes.name = "Katie Holmes"
 holmes.save 
+
+dawesbb = Role.new
+dawesbb.movie_id = bb.id #movie_id
+dawesbb.artist_id = holmes.id #artist_id
+dawesbb.character_name = "Rachel Dawes"
+dawesbb.save
 
 oldman = Artist.new
 oldman.name = "Gary Oldman"
 oldman.save
 
-waynebb = Role.new
-waynebb.movie_id = 1 #movie_id
-waynebb.artist_id = 2 #artist_id
-waynebb.character_name = "Bruce Wayne"
-waynebb.save
-
-alfredbb = Role.new
-alfredbb.movie_id = 1 #movie_id
-alfredbb.artist_id = 3 #artist_id
-alfredbb.character_name = "Alfred"
-alfredbb.save
-
-ghulbb = Role.new
-ghulbb.movie_id = 1 #movie_id
-ghulbb.artist_id = 4 #artist_id
-ghulbb.character_name = "Ra's Al Ghul"
-ghulbb.save
-
-dawesbb = Role.new
-dawesbb.movie_id = 1 #movie_id
-dawesbb.artist_id = 5 #artist_id
-dawesbb.character_name = "Rachel Dawes"
-dawesbb.save
-
 gordonbb = Role.new
-gordonbb.movie_id = 1 #movie_id
-gordonbb.artist_id = 5 #artist_id
+gordonbb.movie_id = bb.id #movie_id
+gordonbb.artist_id = oldman.id #artist_id
 gordonbb.character_name = "Commissioner Gordon"
 gordonbb.save
 
-#dk
+#dk repeating
+waynedk = Role.new
+waynedk.movie_id = dk.id #movie_id
+waynedk.artist_id = bale.id #artist_id
+waynedk.character_name = "Bruce Wayne"
+waynedk.save
 
+#dk new
 ledger = Artist.new
 ledger.name = "Heath Ledger"
 ledger.save
+
+jokerdk = Role.new
+jokerdk.movie_id = dk.id #movie_id
+jokerdk.artist_id = ledger.id #artist_id
+jokerdk.character_name = "Joker"
+jokerdk.save
 
 eckhart = Artist.new
 eckhart.name = "Aaron Eckhart"
 eckhart.save
 
+dentdk = Role.new
+dentdk.movie_id = dk.id #movie_id
+dentdk.artist_id = eckhart.id #artist_id
+dentdk.character_name = "Harvey Dent"
+dentdk.save
+
+#dk repeating
+alfreddk = Role.new
+alfreddk.movie_id = dk.id #movie_id
+alfreddk.artist_id = caine.id #artist_id
+alfreddk.character_name = "Alfred"
+alfreddk.save
+
+#dk new cont.
 gyllenhaal = Artist.new
 gyllenhaal.name = "Maggie Gyllenhaal"
 gyllenhaal.save
 
-waynedk = Role.new
-waynedk.movie_id = 2 #movie_id
-waynedk.artist_id = 2 #artist_id
-waynedk.character_name = "Bruce Wayne"
-waynedk.save
-
-jokerdk = Role.new
-jokerdk.movie_id = 2 #movie_id
-jokerdk.artist_id = 7 #artist_id
-jokerdk.character_name = "Joker"
-jokerdk.save
-
-dentdk = Role.new
-dentdk.movie_id = 2 #movie_id
-dentdk.artist_id = 8 #artist_id
-dentdk.character_name = "Harvey Dent"
-dentdk.save
-
-alfreddk = Role.new
-alfreddk.movie_id = 2 #movie_id
-alfreddk.artist_id = 8 #artist_id
-alfreddk.character_name = "Alfred"
-alfreddk.save
-
 dawesdk = Role.new
-dawesdk.movie_id = 2 #movie_id
-dawesdk.artist_id = 8 #artist_id
+dawesdk.movie_id = dk.id #movie_id
+dawesdk.artist_id = gyllenhaal.id #artist_id
 dawesdk.character_name = "Rachel Dawes"
 dawesdk.save
 
+#dkr repeating
+waynedk = Role.new
+waynedk.movie_id = dkr.id #movie_id
+waynedk.artist_id = bale.id #artist_id
+waynedk.character_name = "Bruce Wayne"
+waynedk.save
+
+gordondk = Role.new
+gordondk.movie_id = dkr.id #movie_id
+gordondk.artist_id = oldman.id #artist_id
+gordondk.character_name = "Commissioner Gordon"
+gordondk.save
 
 #dkr 
 hardy = Artist.new
 hardy.name = "Tom Hardy"
 hardy.save 
 
+banedk = Role.new
+banedk.movie_id = dkr.id #movie_id
+banedk.artist_id = hardy.id #artist_id
+banedk.character_name = "Bane"
+banedk.save
+
 levitt = Artist.new
 levitt.name = "Joseph Gordon-Levitt"
 levitt.save 
+
+blakedk = Role.new
+blakedk.movie_id = dkr.id #movie_id
+blakedk.artist_id = levitt.id #artist_id
+blakedk.character_name = "John Blake"
+blakedk.save
 
 hathaway = Artist.new
 hathaway.name = "Anne Hathaway"
 hathaway.save
 
-waynedk = Role.new
-waynedk.movie_id = 3 #movie_id
-waynedk.artist_id = 2 #artist_id
-waynedk.character_name = "Bruce Wayne"
-waynedk.save
-
-gordondk = Role.new
-gordondk.movie_id = 3 #movie_id
-gordondk.artist_id = 6 #artist_id
-gordondk.character_name = "Commissioner Gordon"
-gordondk.save
-
-banedk = Role.new
-banedk.movie_id = 3 #movie_id
-banedk.artist_id = 10 #artist_id
-banedk.character_name = "Bane"
-banedk.save
-
-blakedk = Role.new
-blakedk.movie_id = 3 #movie_id
-blakedk.artist_id = 11 #artist_id
-blakedk.character_name = "John Blake"
-blakedk.save
-
 kyledk = Role.new
-kyledk.movie_id = 3 #movie_id
-kyledk.artist_id = 12 #artist_id
+kyledk.movie_id = dkr.id #movie_id
+kyledk.artist_id = hathaway.id #artist_id
 kyledk.character_name = "Selina Kyle"
 kyledk.save
 
@@ -263,7 +266,7 @@ kyledk.save
 
 puts "There are #{Movie.all.count} movies. Should be 3" 
 puts "There are #{Artist.all.count} artists. Should be 12"
-puts "There are #{Role.all.count} roles. Should be 10? or 11"
+puts "There are #{Role.all.count} roles. Should be 15."
 
 # Prints a header for the movies output
 puts "Movies"
@@ -277,7 +280,8 @@ movies = Movie.all
 artists = Artist.all
 roles = Role.all
 for movie in movies
-    puts "#{movie.title} #{movie.year_released} #{movie.rating}" + " Christopher Nolan" #fixed this link
+    director = Artist.where({ id: movie.artist_id})[0]
+    puts "#{movie.title} #{movie.year_released} #{movie.rating}  #{director.name}" #fixed this link
 end
 
 
@@ -300,9 +304,16 @@ puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie
 # TODO!
-artists = Artist.all
-for artist in artists
-    for role in roles
-    puts "#{movie.title}, #{artist.name} #{role.character_name}"
-    end
-end
+# artists = Artist.all
+# for artist in artists
+#     for role in roles
+#     puts "#{movie.title}, #{artist.name} #{role.character_name}"
+#     end
+# end
+
+#use this
+for role in roles
+    movie_for_role = Movie.where({ id: role.movie_id})[0]
+    actor = Artist.where({ id: role.artist_id})[0]
+    puts "#{movie_for_role.title} #{actor.name} #{role.character_name}"
+end 
